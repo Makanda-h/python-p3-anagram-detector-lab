f67c4958c80f7297283b672e05cc990c3c7ccc0a
Hope <hopemwaura39@gmail.com>
2024-09-03 16:23:22 +0300

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.word_sorted = sorted(self.word)
+        
+    def match(self, words):
+        return [w for w in words if w.lower() != self.word and sorted(w.lower()) == self.word_sorted]
